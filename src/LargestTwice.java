e436d9eda2b9ded911df7d56704682d2565bcace
elena-elsa101 <elenaelsacherian@gmail.com>
2026-04-06 23:08:04 +0530

    Update LargestTwice.java

--- src/LargestTwice.java
+++ src/LargestTwice.java
@@ -1,35 +1,38 @@
-import java.util.*;
+import java.util.Scanner;
 
-public class Main {
+public class LargestTwice {
+
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-
-        int n = sc.nextInt();
-        int[] nums = new int[n];
-
-        for (int i = 0; i < n; i++) {
-            nums[i] = sc.nextInt();
-        }
-
-        int max = -1;
-        int index = -1;
-
-        // Find largest element and its index
-        for (int i = 0; i < n; i++) {
-            if (nums[i] > max) {
-                max = nums[i];
-                index = i;
+        try (Scanner sc = new Scanner(System.in)) {
+            if (!sc.hasNextInt()) return;
+            int n = sc.nextInt();
+            int[] nums = new int[n];
+            int max = -1;
+            int maxIndex = -1;
+            for (int i = 0; i < n; i++) {
+                if (sc.hasNextInt()) {
+                    nums[i] = sc.nextInt();
+                    if (nums[i] > max) {
+                        max = nums[i];
+                        maxIndex = i;
+                    }
+                }
+            }
+            
+            boolean isValid = true;
+            for (int i = 0; i < n; i++) {
+                if (i != maxIndex && max < 2L * nums[i]) {
+                    isValid = false;
+                    break;
+                }
+            }
+            
+            if (isValid) {
+                System.out.println(maxIndex);
+            } else {
+                System.out.println(-1);
             }
         }
-
-        // Check condition
-        for (int i = 0; i < n; i++) {
-            if (i != index && max < 2 * nums[i]) {
-                System.out.print(-1);
-                return;
-            }
-        }
-
-        System.out.print(index);
     }
 }
+
